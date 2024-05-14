e7e7abfc26dc81ac222398bef5767033e6c55236
jonas majila <118437218+jonasMJL997@users.noreply.github.com>
2024-05-14 23:48:38 +0200

    Add files via upload

---
+++ Age_checker.py.py
@@ -0,0 +1,18 @@
+age = int(input("Enter your age here: "))
+if age >= 18:
+	print("You are older than 18 you can enter the party !")
+
+
+
+import datetime
+Year_of_birth = int(input("In which year you took birth:- "))
+current_year = datetime.datetime.now().year
+Current_age = current_year - Year_of_birth
+print("Your current age is ",Current_age)
+
+if age >= 18:
+	print("congrats you are old enough !")
+
+
+
+
